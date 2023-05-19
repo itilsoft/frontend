3177dfe5bf1f2a8798b6328e6642687dd6b40059
Siraç Petmezçiler <Sirac_ptmzclr@hotmail.com>
2023-05-19 23:35:04 +0300

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Alert,Text, Button, TextInput, View, StyleSheet } from 'react-native';
 
-const LoginScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Login Screen</Text>
-      {/* Diğer bileşenler ve işlemler */}
-    </View>
-  );
-};
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+  
+  onLogin() {
+    const { username, password } = this.state;
 
-export default LoginScreen;
+    Alert.alert('Credentials', `${username} + ${password}`);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textstyle}> Kullanıcı Adı </Text>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(username) => this.setState({ username })}
+          style={styles.input}
+          
+        />
+        <Text style={styles.textstyle}> Şifre </Text>
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <View style={styles.buttonstyle}>
+        <Button
+          title={'Giriş Yap'}
+          onPress={this.onLogin.bind(this)}
+          color= '#ebc8b2'
+        />
+        </View>
+        <View style={styles.buttonstyle}>
+        <Button
+          title={'Hesabım Yok, Kayıt Olmak İstiyorum'}
+          color= '#353535'
+        />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#353535',
+  },
+  input: {
+    width: 300,
+    height: 35,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginBottom: 10,
+    color: "white"
+  },
+  buttonstyle:{
+    width: 300,
+    height: 37,
+    color: '#ebc8b2',
+    borderWidth: 2,
+    overflow: 'hidden',
+    borderRadius: 5,
+    marginBottom: 10,
+    borderColor: '#ebc8b2',
+  },
+  textstyle:{
+    paddingBottom: 5,
+    color: 'white',
+  },
+});
