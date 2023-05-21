b6e0ea4dd8ed418804eec45a38d73d954da133bd
yilmzalpren <yilmzalpren@gmail.com>
2023-05-21 17:12:01 +0300

    login sayfasi tasarimi

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert,Text, Button, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, Text, TouchableOpacity, TextInput, View, StyleSheet, Image } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
@@ -17,36 +17,34 @@ export default class App extends Component {
     Alert.alert('Credentials', `${username} + ${password}`);
   }
 
+  onSignup() {
+    // Kayıt ol butonuna tıklandığında yapılacak işlemler burada olacak.
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textstyle}> Kullanıcı Adı </Text>
+        <Image style={styles.logo} source={require('../assets/logo.png')} />
+        <Text style={styles.label}>Kullanıcı Adı</Text>
         <TextInput
           value={this.state.username}
           onChangeText={(username) => this.setState({ username })}
           style={styles.input}
-          
         />
-        <Text style={styles.textstyle}> Şifre </Text>
+        <Text style={styles.label}>Şifre</Text>
         <TextInput
           value={this.state.password}
           onChangeText={(password) => this.setState({ password })}
           secureTextEntry={true}
           style={styles.input}
         />
-        <View style={styles.buttonstyle}>
-        <Button
-          title={'Giriş Yap'}
-          onPress={this.onLogin.bind(this)}
-          color= '#ebc8b2'
-        />
-        </View>
-        <View style={styles.buttonstyle}>
-        <Button
-          title={'Hesabım Yok, Kayıt Olmak İstiyorum'}
-          color= '#353535'
-        />
-        </View>
+        <TouchableOpacity style={styles.loginButton} onPress={this.onLogin.bind(this)}>
+          <Text style={styles.loginButtonText}>Giriş Yap</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.signupButton} onPress={this.onSignup.bind(this)}>
+          <Text style={styles.signupButtonText}>Hesabım Yok, Kayıt Olmak İstiyorum</Text>
+        </TouchableOpacity>
+        <Image style={styles.bottomImage} source={require('../assets/logo.png')} />
       </View>
     );
   }
@@ -56,30 +54,61 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#353535',
+    justifyContent: 'flex-start',
+    backgroundColor: 'black',
+    paddingTop: 200, // Eklendi
+    padding: 15,
+  },
+  logo: {
+    position: 'absolute',
+    top: 50, // Üst logo için konumlandırmayı ayarla
+    left: 0,
+    width: 100, //Logo boyutlarını ayarlayın
+    height: 100,
+  },
+  label: {
+    width: '100%',
+    color: '#fff',
   },
   input: {
-    width: 300,
-    height: 35,
+    width: '100%',
+    height: 50,
     padding: 10,
-    borderWidth: 1,
-    borderColor: 'white',
+    backgroundColor: '#353535',
+    color: '#fff',
     marginBottom: 10,
-    color: "white"
+    borderBottomColor: 'grey', // TextInput border rengini gri yaptık
+    borderBottomWidth: 1, // ve kalınlığını 1 olarak belirledik
   },
-  buttonstyle:{
-    width: 300,
-    height: 37,
-    color: '#ebc8b2',
+  loginButton: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  loginButtonText: {
+    color: '#000',
+    textAlign: 'center',
+  },
+  signupButton: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#353535',
+    marginBottom: 10,
+    borderColor: 'grey',
     borderWidth: 2,
-    overflow: 'hidden',
-    borderRadius: 5,
-    marginBottom: 10,
-    borderColor: '#ebc8b2',
+    marginTop: 10,
   },
-  textstyle:{
-    paddingBottom: 5,
-    color: 'white',
+  signupButtonText: {
+    color: 'yellow',
+    textAlign: 'center',
+  },
+  bottomImage: {
+    position: 'absolute',
+    bottom: 50, // Alt resim için konumlandırmayı ayarla
+    right: 0,
+    width: 100, //Alt resim boyutlarını ayarlayın
+    height: 100,
   },
 });
